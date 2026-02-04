4c8cdc74e08bada2678d3e8e10a08ef64a6b6330
Adrian Obregon <adriancr93@hotmail.com>
2026-02-03 19:26:42 -0600

    Create DashBoard

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,5 +7,15 @@ export const projectsSchema = z.object({
     clientName: z.string(),
     description: z.string()
 })
+
+export const dashboardProjectsSchema = z.array(
+    projectsSchema.pick({
+        _id: true,
+        projectName: true,
+        clientName: true,
+        description: true
+    })
+)
+
 export type Project = z.infer<typeof projectsSchema>
 export type ProjectFormData = Pick<Project, 'projectName' | 'clientName' | 'description'>
