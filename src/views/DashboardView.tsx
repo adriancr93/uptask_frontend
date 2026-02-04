43ce7e0930a9e2b12db2037d606df4088e706487
Adrian Obregon <adriancr93@hotmail.com>
2026-02-03 21:14:12 -0600

    Update and Delete projects

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -2,16 +2,28 @@ import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
 import { Link } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import { getProjects } from "@/api/ProjectAPI";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteProject, getProjects } from "@/api/ProjectAPI";
+import { toast } from "react-toastify";
 
 export default function DashboardView() {
   const { data, isLoading } = useQuery({
     queryKey: ["projects"],
     queryFn: getProjects,
   });
+  
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation({
+    mutationFn: deleteProject,
+    onError: (error) => {
+      toast.error(error.message)
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["projects"] })
+      toast.success(data)
+    }
+  })
 
-  console.log(data);
   if (isLoading) return "Loading...";
 
   if (data)
@@ -88,7 +100,7 @@ export default function DashboardView() {
                         </Menu.Item>
                         <Menu.Item>
                           <Link
-                            to={``}
+                            to={`/projects/${project._id}/edit`}
                             className="block px-3 py-1 text-sm leading-6 text-gray-900"
                           >
                             Edit Project
@@ -98,7 +110,7 @@ export default function DashboardView() {
                           <button
                             type="button"
                             className="block px-3 py-1 text-sm leading-6 text-red-500"
-                            onClick={() => {}}
+                            onClick={() => mutate(project._id)}
                           >
                             Delete Project
                           </button>
---
+++ src/views/projects/EditProjectView.tsx
@@ -0,0 +1,19 @@
+import { Navigate, useParams } from "react-router"
+import { useQuery } from "@tanstack/react-query"
+import { getProjectsById } from "@/api/ProjectAPI";
+import EditProjectForm from "@/components/projects/EditProjectForm";
+
+export default function EditProjectView() {
+   const params = useParams()
+   const projectId = params.projectId!
+   
+    const { data, isLoading, error, isError } = useQuery({
+        queryKey: ["editProject", projectId], // Added projectId to queryKey for uniqueness
+        queryFn: () => getProjectsById(projectId),
+        retry: false
+    });
+   
+    if(isLoading) return <div>Loading...</div>;
+    if(isError) return <Navigate to='/404' />
+    if(data) return <EditProjectForm data={data} projectId={projectId} />
+}
